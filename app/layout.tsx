5989dc87ad709c03b68da0136dfa89c90299c44a
a96363877 <a96363877@gmail.com>
2025-04-26 18:12:21 +0300

    xsc

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { CartProvider } from "@/context/cart-context"
 import { Toaster } from "@/components/ui/toaster"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
+import Script from "next/script"
 
 // Use Cairo font for better Arabic support
 const cairo = Cairo({
@@ -29,6 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" className={cairo.variable}>
+         
       <body className="font-cairo min-h-screen flex flex-col bg-white">
         <CartProvider>
           <Header/>
@@ -36,6 +38,15 @@ export default function RootLayout({
           <Footer/>
           <Toaster />
         </CartProvider>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=AW-17036437498`} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17036437498');
+          `}
+        </Script>
       </body>
     </html>
   )
